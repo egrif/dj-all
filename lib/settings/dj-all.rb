e1d8f4bb7a37f8f76e0e56cc601fd04cf76bb91f
Eric Griffith <eric.griffith@greenhouse.io>
2021-11-10 15:32:54 -0500

    add config file

--- lib/settings/dj-all.rb
+++ lib/settings/dj-all.rb
@@ -1,33 +1,12 @@
 require 'ostruct'
+require 'yaml'
 require 'json'
 
 module Settings
-  DJALL = JSON.parse({
-    yaml: {
-      ttl: 600,
-      default_folder: '/tmp'
-    },
-
-    groups: {
-      greenhouse: {
-        all: 'prod,canary,use1|prod,prod,use1|prod,prod-s2,use1|prod,prod-s3,use1|prod,prod-s4,use1|prod,prod-s101,euc1',
-        us: 'prod,canary,use1|prod,prod,use1|prod,prod-s2,use1|prod,prod-s3,use1|prod,prod-s4,use1',
-        us_base: 'prod,canary,use1|prod,prod,use1|prod,prod-s2,use1|prod,prod-s3,use1|prod,prod-s4,use1|prod,prod-base,use1',
-        legacy: 'prod,canary,use1|prod,prod,use1|prod,prod-s2,use1',
-        leaders: 'prod,prod,use1|prod,prod-s101,euc1',
-        staging: 'prod,staging,use1|prod,staging-canary,use1|prod,staging-s2,use1',
-        staging_base: 'prod,staging,use1|prod,staging-canary,use1|prod,staging-s2,use1|prod,staging-base,use1',
-        non_prod: 'prod,staging,use1|prod,staging-canary,use1|prod,staging-s2,use1|prod,sandbox,use1',
-        non_prod_base: 'prod,staging,use1|prod,staging-canary,use1|prod,staging-s2,use1|prod,sandbox,use1'
-      }
-    },
-
-    formatting: {
-      column_spacing: 2,
-      max_display_width: 180
-    },
-
-    secret_password: ENV.fetch('DJ_ALL_SECRET_PASSWORD', nil)
-  }.to_json, object_class: OpenStruct)
-
+  DJALL = JSON.parse(
+    YAML.load(File.read(File.join(__dir__, 'dj-all.yml'))).merge(
+      File.file?(ENV.fetch('DJALL_CONFIG_FILE',"#{ENV['HOME']}/.dj-all.yml")) ? YAML.load(File.read(ENV.fetch('DJALL_CONFIG_FILE',"#{ENV['HOME']}/.dj-all.yml"))) : {}
+    ).merge({
+      secret_password: ENV.fetch('DJ_ALL_SECRET_PASSWORD', nil)
+    }).to_json, object_class: OpenStruct)
 end
